deda672b7cda038fcfdc1cb7d3a1ea872079d583
index-git <66255344+index-git@users.noreply.github.com>
2020-09-30 11:55:31 +0200

    Create WFS proxy in test client (#9)
    Closes https://github.com/jirik/layman/issues/101

---
+++ components/WfsPostTransactionParams.js
@@ -0,0 +1,86 @@
+import {Form, Button} from 'semantic-ui-react'
+import htmlCleaner from "clean-html";
+
+const get_file_content = async (file) => {
+  const fr = new FileReader();
+  return new Promise((resolve, reject) => {
+    fr.onload = (evt) => {
+      resolve(evt.target.result);
+    };
+    fr.onerror = reject;
+    fr.readAsText(file);
+  });
+}
+
+
+
+class WfsPostTransactionParams extends React.PureComponent {
+
+  fileInputRef = React.createRef();
+
+  fileChange = async e => {
+      const file = e.target.files[0];
+      const file_content = await get_file_content(file);
+      this.props.handleDataChange(file_content);
+  };
+
+  render() {
+    const {user} = this.props;
+    const sampleXML = `<?xml version="1.0"?>
+<wfs:Transaction
+   version="2.0.0"
+   service="WFS"
+   xmlns:username="http://${user}"
+   xmlns:fes="http://www.opengis.net/fes/2.0"
+   xmlns:gml="http://www.opengis.net/gml/3.2"
+   xmlns:wfs="http://www.opengis.net/wfs/2.0"
+   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+   xsi:schemaLocation="http://www.opengis.net/wfs/2.0
+                       http://schemas.opengis.net/wfs/2.0/wfs.xsd
+                       http://www.opengis.net/gml/3.2
+                       http://schemas.opengis.net/gml/3.2.1/gml.xsd">
+   <wfs:Insert>
+       <username:populated_places>
+           <username:wkb_geometry>
+               <gml:MultiCurve srsName="urn:ogc:def:crs:EPSG::3857" srsDimension="2">
+                   <gml:curveMember>
+                       <gml:LineString>
+                           <gml:posList>3722077.1689 5775850.1007 3751406.9331 5815606.0102 3830548.3984 5781176.5357
+                               3866350.4899 5774848.8358 3880796.9478 5743277.797 3897591.3679 5738418.6547
+                           </gml:posList>
+                       </gml:LineString>
+                   </gml:curveMember>
+               </gml:MultiCurve>
+           </username:wkb_geometry>
+       </username:populated_places>
+   </wfs:Insert>
+</wfs:Transaction>`
+    return (
+        <div>
+          <Form.TextArea
+              className="mandatory"
+              name="data"
+              label="XML Body"
+              placeholder={sampleXML}
+              rows={15}
+              value={this.props.data}
+              onChange={(event) => {this.props.handleDataChange(event.target.value)}}/>
+          <Button
+              content="Import XML from file"
+              onClick={() => this.fileInputRef.current.click()}/>
+          <input
+              ref={this.fileInputRef}
+              hidden
+              name="xml"
+              type="file"
+              onChange={this.fileChange}/>
+          <Button content="Insert sample XML (layername=populated_places)" onClick={(event) => {this.props.handleDataChange(sampleXML)}}/>
+          <br/>
+          <br/>
+        </div>
+    );
+  }
+}
+
+export default WfsPostTransactionParams;
+
---
+++ pages/wfs.js
@@ -0,0 +1,284 @@
+import HeaderMenu from './../components/HeaderMenu'
+import {Container, Form, Button, Header, Table, Ref, Icon, Segment, Message, Progress} from 'semantic-ui-react'
+import fetch from 'unfetch';
+import ReactDOM from 'react-dom';
+import WfsPostTransactionParams from "../components/WfsPostTransactionParams";
+import scrollIntoView from 'scroll-into-view';
+import UserPathParams from "../components/UserPathParams";
+import getConfig from 'next/config'
+import xmlFormatter from 'xml-formatter'
+import htmlCleaner from 'clean-html'
+const { publicRuntimeConfig } = getConfig();
+
+const ASSET_PREFIX = publicRuntimeConfig.ASSET_PREFIX;
+
+const containerStyle = {
+  position: 'absolute',
+  top: '40px',
+  padding: '1em',
+};
+
+const toTitleCase = (str) => {
+    return str.replace(/\w\S*/g, (txt) => {
+        return txt.charAt(0).toUpperCase() + txt.substr(1);
+    });
+};
+
+const getRequestTitle = (request) => {
+  const parts = request.split('-')
+  parts[0] = parts[0].toUpperCase();
+  const title = toTitleCase(parts.join(' '));
+  return title;
+}
+
+const requestToParamsClass = {
+  'post-transaction': WfsPostTransactionParams,
+}
+
+const endpointToUrlPartGetter = {
+  'transaction': ({user}) => `/${user}/wfs`,
+}
+
+const endpointToPathParamsClass = {
+  'transaction': UserPathParams,
+}
+
+const getEndpointDefaultParamsState = (endpoint, state) => {
+  const getters = {}
+  const getter = getters[endpoint];
+  return getter ? getter(state) : {};
+}
+
+const getEndpointParamsProps = (endpoint, component) => {
+  const user_props = {
+    user: component.state.user,
+    data: component.state.data,
+    handleUserChange: component.handleUserChange.bind(component),
+    handleDataChange: component.handleDataChange.bind(component),
+  };
+  const props = {
+    'transaction': user_props,
+  }
+  return props[endpoint];
+}
+
+const requestToEndpoint = (request) => {
+  const parts = request.split('-')
+  parts.shift();
+  return parts.join('-');
+}
+
+const requestToQueryParamValues = {
+  'post-transaction': {
+    'request': 'Transaction',
+  },
+}
+
+const cleanHtml = async (text) => {
+  return new Promise((resolve, reject) => {
+    const result = htmlCleaner.clean(text, resolve);
+  });
+}
+
+class WFSPage extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: props.user && props.user.username ? props.user.username : 'browser',
+      request: 'post-transaction',
+      data: '',
+      response: null,
+    };
+    this.formEl;
+    this.respRef = React.createRef();
+  }
+
+  handleUserChange(event) {
+    this.setState({user: event.target.value});
+  }
+
+  handleDataChange(data) {
+    this.setState({data});
+  }
+
+  setRequest(request) {
+    const endpoint = requestToEndpoint(request);
+    this.setState({
+      ...getEndpointDefaultParamsState(endpoint, this.state),
+      request,
+      response: null
+    });
+  }
+
+  handleSubmitClick(event) {
+    this.setState({response: null});
+
+    const response = {};
+
+    const fetchOpts = {
+      method: "POST",
+      body: this.state.data,
+      headers: {
+        "Content-Type": "text/xml",
+        "Accept": "text/xml"}
+    };
+
+    const queryParamsOrig = requestToQueryParamValues[this.state.request] || {};
+    // Clone param dictionary, so we do not change it
+    const queryParams = JSON.parse(JSON.stringify(queryParamsOrig));
+
+    queryParams.timestamp = (+new Date());
+    const query_part = Object.keys(queryParams).map(k => {
+      const v = queryParams[k];
+      return encodeURIComponent(k) + "=" + encodeURIComponent(v);
+    }).join('&');
+
+    const url_path = this.getRequestUrlPath() + '?' + query_part;
+
+    fetch(url_path, fetchOpts).then((r) => {
+      response.status = r.status;
+      response.ok = r.ok;
+      response.contentType = r.headers.get('content-type');
+      return r.text();
+    }).then( async (text) => {
+      // authentication failed
+      if(response.status === 403 && response.json && response.json.code === 32) {
+        // await sleep(1000);
+        await this.props.handle_authn_failed();
+      }
+      let pretty_text = "";
+      if (response.contentType.includes("/json")) {
+        response.json = JSON.parse(text);
+        pretty_text = JSON.stringify(response.json, null, 2);
+      } else if (response.contentType.includes("/xml")) {
+        pretty_text = xmlFormatter(text)
+      } else if (response.contentType.includes("/html")) {
+        pretty_text = await cleanHtml(text);
+      } else {
+        pretty_text = text;
+      }
+      response.pretty_text = pretty_text;
+
+    }).finally(() => {
+      this.setState({response});
+      const domNode = ReactDOM.findDOMNode(this.respRef.current)
+      if(domNode) {
+        scrollIntoView(domNode);
+      }
+    });
+
+  }
+
+  handleFormRef(formElement) {
+    this.formEl = formElement;
+  }
+
+  getRequestUrlPath() {
+    const endpoint = requestToEndpoint(this.state.request);
+    const getter = endpointToUrlPartGetter[endpoint];
+    const urlPart = getter ? getter(this.state) : '';
+    const url = `${ASSET_PREFIX}/geoserver${urlPart}`;
+    return url
+  }
+
+  render() {
+    const {response} = this.state;
+    let respEl = null;
+    if(response) {
+      const resp_body= <code style={{whiteSpace: 'pre'}}>{response.pretty_text}</code>
+
+      respEl =
+          <div ref={this.respRef}>
+            <Segment>
+              <Message positive={response.ok}  negative={!response.ok}>
+                <Message.Header>Response</Message.Header>
+                <p>Status code: {response.status}</p>
+                <p>Content-Type: {response.contentType}</p>
+                {resp_body}
+              </Message>
+            </Segment>
+          </div>
+    }
+
+    const endpoint = requestToEndpoint(this.state.request);
+    const paramsProps = getEndpointParamsProps(endpoint, this);
+    const pathParamsClass = endpointToPathParamsClass[endpoint];
+    let pathParams = null;
+    if (pathParamsClass) {
+      pathParams = React.createElement(
+          pathParamsClass,
+          paramsProps
+      )
+    }
+
+    const paramsClass = requestToParamsClass[this.state.request];
+    let params = null;
+    if (paramsClass) {
+      params = React.createElement(
+          paramsClass,
+          paramsProps
+      )
+    }
+
+    const renderEndpointTable = (
+            <Table celled>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Request</Table.HeaderCell>
+                  <Table.HeaderCell>URL</Table.HeaderCell>
+                  <Table.HeaderCell>GET</Table.HeaderCell>
+                  <Table.HeaderCell>POST</Table.HeaderCell>
+                  <Table.HeaderCell>PATCH</Table.HeaderCell>
+                  <Table.HeaderCell>DELETE</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell>Transaction</Table.Cell>
+                  <Table.Cell><code>/geoserver/&lt;user&gt;/wfs</code></Table.Cell>
+                  <Table.Cell>x</Table.Cell>
+                  <Table.Cell>
+                    <Button
+                        toggle
+                        active={this.state.request === 'post-transaction'}
+                        onClick={this.setRequest.bind(this, 'post-transaction')}
+                    >POST</Button>
+                  </Table.Cell>
+                  <Table.Cell>x</Table.Cell>
+                  <Table.Cell>x</Table.Cell>
+                </Table.Row>
+          </Table.Body>
+        </Table>
+        )
+
+    return (
+        <div>
+          <HeaderMenu user={this.props.user} show_log={!!this.props.num_authn_providers} />
+
+          <Container style={containerStyle}>
+            <Header as='h1'>Test Client of Layman WFS Endpoint</Header>
+            <p>
+              <a href="https://github.com/jirik/layman/blob/master/doc/endpoints.md#web-feature-service"
+                 target="_blank">Layman WFS Endpoint Documentation</a>
+            </p>
+            <Header as='h2'>Requests and Actions</Header>
+            {renderEndpointTable}
+            <Header as='h2'>{getRequestTitle(this.state.request)}</Header>
+            <Header as='h3'>Parameters</Header>
+            <Ref innerRef={this.handleFormRef.bind(this)}>
+              <Form>
+                {pathParams}
+                {params}
+                <Button primary type='submit' onClick={this.handleSubmitClick.bind(this)}>Submit</Button>
+              </Form>
+            </Ref>
+            {respEl}
+          </Container>
+        </div>
+    );
+  }
+}
+
+export default WFSPage;
+
