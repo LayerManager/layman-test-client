2f350890d29e5e51447ca96a0fc6ebfb9f894747
Jiri Kozel <jirik@users.noreply.github.com>
2021-09-24 19:07:49 +0200

    Prepare state related to style in PostWorkspaceLayersParams

--- components/PostWorkspaceLayersParams.js
+++ components/PostWorkspaceLayersParams.js
@@ -1,9 +1,30 @@
+import assert from 'assert';
 import {Form} from 'semantic-ui-react'
 import PublicationAccessRightsParams from "./PublicationAccessRightsParams";
 
+const NO_STYLE_CHOSEN = 'no_style_chosen';
+const STYLE_CHOSEN_AND_PENDING = 'style_chosen_and_pending';
+const STYLE_CHOSEN_AND_ERROR = 'style_chosen_and_error';
+const STYLE_CHOSEN_AND_LOADED = 'style_chosen_and_loaded';
+const STYLE_STATES = [
+    NO_STYLE_CHOSEN,
+    STYLE_CHOSEN_AND_PENDING,
+    STYLE_CHOSEN_AND_ERROR,
+    STYLE_CHOSEN_AND_LOADED,
+]
+
 class PostWorkspaceLayersParams extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      style: NO_STYLE_CHOSEN,
+    };
+  }
 
   render() {
+    const style_state = this.state.style;
+    assert(STYLE_STATES.includes(style_state));
+    let style_ui = null;
     return (
         <div>
           <Form.Field inline className="mandatory">
@@ -35,6 +56,7 @@ class PostWorkspaceLayersParams extends React.PureComponent {
             <label>Style file</label>
             <input name="style" type="file" accept=".sld,.xml,.qml"/>
           </Form.Field>
+          {style_ui}
           <PublicationAccessRightsParams/>
         </div>
     );
